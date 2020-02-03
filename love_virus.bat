f0c3dee53031eb088bced4a8d507965c1f9280f2
Ankit Kaushal <55594118+ankit-kaushal@users.noreply.github.com>
2020-02-04 00:53:42 +0530

    Virus
    Love Virus
    Actually its just a Prank.

--- love_virus.bat
+++ love_virus.bat
@@ -3,14 +3,14 @@ color 30
 echo Hey, do you love me (only answer in yes or no)
 set /p love=
 if %love%==yes goto love
-if %love%==no goto hate
+if %love%==no goto notlove
 :love
 echo I love you too...
 echo Meet you soon :)
 pause 
 exit
-:hate
-echo But I love you....hehehehehe
+:notlove
+echo I love you but you broke my heart....
 echo You are hacked...
 echo Your PC will crash in 10 seconds
 timeout 10
